bb4f20f5536139cd92054c6721d8a5fc5c20c54a
mlraleader <mouwenlegan@gmail.com>
2019-02-18 14:35:19 +0100

    Ajout du s6.js

--- sprint/s6.js
+++ sprint/s6.js
@@ -2,6 +2,6 @@ let usrAge = Number.parseInt(prompt("Entrez votre age :"));
 if(usrAge<21){
     console.log("Vous etes mineur");
 }
-else if(usrAge>21){
+else if(usrAge>=21){
     console.log("Vous etes majeur");
 }
