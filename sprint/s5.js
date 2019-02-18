73a3548651821b64cc69b5483977ed584cad332d
mlraleader <mouwenlegan@gmail.com>
2019-02-18 14:20:38 +0100

    Ajout du s5.js

---
+++ sprint/s5.js
@@ -0,0 +1,8 @@
+var usrNbr1 = Number.parseInt(prompt("Entrez un nombre:"));
+var usrNbr2 = Number.parseInt(prompt("Entrez un nombre:"));;
+if(usrNbr1>usrNbr2){
+    console.log(usrNbr1+ " est supérieur.");
+}
+else if(usrNbr1<usrNbr2) {
+    console.log(usrNbr2+ "est supérieur.");
+}
