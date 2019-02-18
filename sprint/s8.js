c880d9e6661a90245d13d8ee09c883678b486bdc
mlraleader <mouwenlegan@gmail.com>
2019-02-18 16:17:51 +0100

    Ajout du s8.js

--- sprint/s8.js
+++ sprint/s8.js
@@ -2,9 +2,9 @@ let nbr;
 let i;
 nbr = Number.parseInt(prompt("Entrez un nombre :"));
 if(nbr){
-for(i=0;i<=10;i++){
-    console.log("Input: "+nbr);
+    console.log("Input: " + nbr);
     console.log("Output:");
+for(i=0;i<=10;i++){
     console.log(nbr+ "x"+i+" = "+nbr*i);
     }
 }
