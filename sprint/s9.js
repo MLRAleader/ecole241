e9a16b05fb202077e43441bd0c0001e8953bb4eb
mlraleader <mouwenlegan@gmail.com>
2019-02-21 10:50:20 +0100

    Plate form-educ

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,14 +1,13 @@
-
 var i;
-var tab=[0];
-var max=tab[0];
+var tab = [0];
+var max = tab[0];
 console.log("Input");
-for(i=1;i<=5;i++){
-   usrIpt=Number.parseInt(prompt("Entrez le nombre numéro "+i+":"));
-   tabIn=tab.push(usrIpt);
-   console.log("Entrez le numéro "+i+" :"+usrIpt);
-    if(tab[i]>max){
-        max=tab[i];
+for (i = 1; i <= 5; i++) {
+    usrIpt = Number.parseInt(prompt("Entrez le nombre numéro " + i + ":"));
+    tabIn = tab.push(usrIpt);
+    console.log("Entrez le numéro " + i + " :" + usrIpt);
+    if (tab[i] > max) {
+        max = tab[i];
     }
 }
-console.log("Output\n Le plus grand de ces nombre est :"+max);
+console.log("Output\n Le plus grand de ces nombre est :" + max);
