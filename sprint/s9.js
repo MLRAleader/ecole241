e7c94f0831351c561f50e30c3e6fc596cb61cc73
mlraleader <mouwenlegan@gmail.com>
2019-02-25 17:05:22 +0100

    Otimisation s9.js

--- sprint/s9.js
+++ sprint/s9.js
@@ -3,8 +3,6 @@ console.log("Input");
 for (var i = 1; i <= 5; i++) {
     usrIpt = Number.parseInt(prompt("Entrez le nombre numéro " + i + ":"));
     console.log("Entrez le numéro " + i + " :" + usrIpt);
-    if (usrIpt > max) {
-        max = usrIpt;
-    }
+    if (usrIpt > max) max = usrIpt;
 }
 console.log("Output\n Le plus grand de ces nombre est :" + max);
