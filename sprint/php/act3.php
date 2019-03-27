73a53f65a9f252d86e3c5ff15c618820425b7d39
mlraleader <mouwenlegan@gmail.com>
2019-03-27 10:21:35 +0100

    Creation du tableau

---
+++ sprint/php/act3.php
@@ -0,0 +1,7 @@
+<?php 
+ $tab=array("Fatansia","Lolo","LaRoma","Pitchu","Talita","Kombonangay","LeFeu","Kitika","Kamika","Zapata");
+foreach ($tab as $key => $value) {
+    echo $value .'&nbsp;';
+} 
+?>
+
