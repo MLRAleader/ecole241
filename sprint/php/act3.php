983435a4c40a446533edef9d49009434616f2c8e
mlraleader <mouwenlegan@gmail.com>
2019-03-27 11:28:14 +0100

    Optimisation du act3

--- sprint/php/act3.php
+++ sprint/php/act3.php
@@ -1,7 +1,7 @@
 <?php 
  $tab=array("Fatansia","Lolo","LaRoma","Pitchu","Talita","Kombonangay","LeFeu","Kitika","Kamika","Zapata");
-foreach ($tab as $key => $value) {
-    echo $value .'&nbsp;';
+foreach ($tab as $value) {
+    echo $value .'&nbsp;&nbsp;';
 } 
 ?>
 
